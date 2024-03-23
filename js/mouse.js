c7d616844fb774aed8092333201f560b76c009fe
Leo <leocabbage2008@gmail.com>
2024-03-23 00:26:11 -0400

    undone

--- js/mouse.js
+++ js/mouse.js
@@ -7,10 +7,23 @@ const newParticle = (e) => {
   particle.classList.add("particle", "active");
   return particle
 }
+const newSVG = (x1, y1, x2, y2) => {
+  const newLine = document.createElementNS('http://www.w3.org/2000/svg', 'line');
+  newLine.setAttribute("class", "line")
+  newLine.setAttribute('x1', x1);
+  newLine.setAttribute('y1', y1);
+  newLine.setAttribute('x2', x2);
+  newLine.setAttribute('y2', y2);
+  return newLine;
+}
+document.getElementById("canvas").append(newSVG(0, 0, 100, 100));
+
 document.addEventListener('mousemove', (e) => {
   trail = Array.from(document.getElementsByClassName("particle"))
   trail.unshift(newParticle(e))
   for (const i of trail.slice(SIZE)) document.body.removeChild(i)
   trail = trail.slice(0, SIZE)
-  for (const i of trail) document.body.appendChild(i)
+  for (const i of trail) {
+    document.body.appendChild(i)
+  }
 }, false)
