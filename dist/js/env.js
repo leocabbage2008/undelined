8860d3140c0f3ef4d7c39b76f74a95ed6048fe47
Leo <leocabbage2008@gmail.com>
2024-05-06 23:15:21 -0400

    path change

--- dist/js/env.js
+++ dist/js/env.js
@@ -3,7 +3,7 @@
 var __webpack_exports__ = {};
 
 ;// CONCATENATED MODULE: ./info.json
-const info_namespaceObject = /*#__PURE__*/JSON.parse('{"result":"success","info":{"sitename":"undelined","views":1739,"hits":3439,"created_at":"Sun, 28 Jan 2024 20:58:03 -0000","last_updated":"Mon, 06 May 2024 00:11:18 -0000","domain":null,"tags":["blog","retro"],"timeAccessed":"2024-05-07T02:42:27.194Z"}}');
+const info_namespaceObject = /*#__PURE__*/JSON.parse('{"result":"success","info":{"sitename":"undelined","views":1739,"hits":3439,"created_at":"Sun, 28 Jan 2024 20:58:03 -0000","last_updated":"Mon, 06 May 2024 00:11:18 -0000","domain":null,"tags":["blog","retro"],"timeAccessed":"2024-05-07T03:15:12.267Z"}}');
 ;// CONCATENATED MODULE: ./js/env.js
 
 document.getElementById("env").innerHTML = JSON.stringify(info_namespaceObject);
