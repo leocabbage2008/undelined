c474eba863400b3e20334b9f10356897b92323ac
Leo <leocabbage2008@gmail.com>
2024-05-14 18:54:02 -0400

    rerun because python

--- cleanmusic.py
+++ cleanmusic.py
@@ -1,9 +1,10 @@
-import os, glob, shutil
+# because no more music
+# import os, glob, shutil
 
-try:
-    shutil.rmtree("./music/.Trashes")
-except:
-    pass
-for f in os.listdir("./music"):
-    if f[0] == ".":
-        os.remove("./music/" + f)
+# try:
+#     shutil.rmtree("./music/.Trashes")
+# except:
+#     pass
+# for f in os.listdir("./music"):
+#     if f[0] == ".":
+#         os.remove("./music/" + f)
