2017678dc3d6b48f95d3ea70ca38724f9ba8a093
Leo <leocabbage2008@gmail.com>
2024-12-03 21:41:22 -0500

    photo

--- dist/js/bundle.js
+++ dist/js/bundle.js
@@ -838,7 +838,7 @@ var __webpack_exports__ = {};
 (() => {
 
 ;// CONCATENATED MODULE: ./info.json
-const info_namespaceObject = /*#__PURE__*/JSON.parse('{"info":{"sitename":"undelined","views":4233,"hits":12079,"created_at":"Sun, 28 Jan 2024 20:58:03 -0000","last_updated":"Mon, 15 Jul 2024 20:26:02 -0000","domain":null,"tags":["blog","retro","sunset"],"timeAccessed":"2024-09-23T03:00:25.368Z"},"commit":{"message":"oopsies dotenv sowwy","stats":{"total":19,"additions":17,"deletions":2},"filesChanged":2,"time":"2024-07-15T20:25:39Z"},"run":{"start":"2024-07-15T20:25:46Z","length":20,"status":"completed","event":"push","run_number":92}}');
+const info_namespaceObject = /*#__PURE__*/JSON.parse('{"info":{"sitename":"undelined","views":4527,"hits":13593,"created_at":"Sun, 28 Jan 2024 20:58:03 -0000","last_updated":"Mon, 23 Sep 2024 03:01:59 -0000","domain":null,"tags":["blog","retro","sunset"],"timeAccessed":"2024-10-13T19:30:13.666Z"},"commit":{"message":"blog","stats":{"total":1920,"additions":1909,"deletions":11},"filesChanged":9,"time":"2024-09-23T03:00:32Z"},"run":{"start":"2024-09-23T03:01:30Z","length":32,"status":"completed","event":"push","run_number":93}}');
 ;// CONCATENATED MODULE: ./js/info.js
 
 
